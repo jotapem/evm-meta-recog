c06300fce1ddcff84b90167392fafe64fe718ac7
José Pedro Martinez <brightjotaxiv@gmail.com>
2018-04-26 16:09:28 -0300

    ADD: own KNN extension so interface matches EVM (which I found simpler to work with)

---
+++ recognition/estimator/knn.py
@@ -0,0 +1,15 @@
+from sklearn.neighbors import KNeighborsClassifier as sk_KNN
+
+class KNN(sk_KNN):
+    def __init__(self, *a, **kw):
+        super().__init__(*a, **kw)
+
+    def predict_with_prop(self, X):
+        Y_ = self.predict(X)
+        Y_kdist, Y_kindexes = self.kneighbors(X)
+
+        
+        Y_better = list(map(lambda i: [Y_[i], str(Y_kdist[i][0])], range(len(Y_kdist))))
+            
+        return Y_better
+            
