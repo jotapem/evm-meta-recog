45118258f1fa4de24db64a1ed3dbf1c34fa71511
José Pedro Martinez <brightjotaxiv@gmail.com>
2018-04-27 14:47:36 -0300

    FIX: unknown logic on KNN eval fixed ; ADD: evaluating predictions from all experiments

--- recognition/trainer/results/csv_eval.py
+++ recognition/trainer/results/csv_eval.py
@@ -1,3 +1,4 @@
+import operator
 import os
 import csv
 
@@ -65,6 +66,11 @@ def eval_evm(content)->dict:
         'tp':0, 'fp': 0,
         'tn':0, 'fn': 0
     }
+    mismatches = {} # key is a class
+    def count_mm(k):
+        if k not in mismatches.keys():
+            mismatches[k] = 0
+        mismatches[k] += 1
 
     for row in content:
         counter['total'] += 1
@@ -79,6 +85,10 @@ def eval_evm(content)->dict:
                 counter['tp'] += 1
             else:
                 counter['fp'] += 1
+                count_mm(row['prediction'])
+
+    mismatches = sorted(mismatches.items(), key=operator.itemgetter(1))
+    print(mismatches)
 
     assert counter['total'] == sum([counter['tp'], counter['fp'], counter['tn'], counter['fn']])
     return relative_counter(counter)
@@ -94,9 +104,9 @@ def eval_knn(content, p_threshold:float)->dict:
         counter['total'] += 1
 
         dist = float(row['value'])
-        p = dist / 0.6
+        p = dist #/ 0.6
 
-        if p < p_threshold: # classifier output is negative
+        if p > p_threshold: # classifier output is negative
             if row['truth'] == _UK:
                 counter['tn'] += 1
             else:
@@ -113,50 +123,51 @@ def eval_knn(content, p_threshold:float)->dict:
 def main():
     results_path = os.path.join('recognition', 'trainer', 'results')
 
-    gallery = load_content(os.path.join(results_path, '10pp', 'gallery_test.csv'))
-    hidden = hide_truth(load_content(os.path.join(results_path, '10pp', 'hidden_test.csv')))
+    for exp in ['10pp', '20pp', '50pp']:
+        gallery = load_content(os.path.join(results_path, exp, 'gallery_test.csv'))
+        hidden = hide_truth(load_content(os.path.join(results_path, '10pp', 'hidden_test.csv')))
 
-    OSTs = [0.0, 0.01, 0.05, 0.1, 0.2]
-    evm_names = list(map(lambda x: "EVM%dm" % (x*1000), OSTs))
-    print(evm_names)
+        OSTs = [0.0, 0.01, 0.05, 0.1, 0.2]
+        evm_names = list(map(lambda x: "EVM%dm" % (x*1000), OSTs))
+        print(evm_names)
 
-    hidden_content, gallery_content = {'KNN': [], 'EVM': []}, {'KNN': [], 'EVM': []}
-    for i in range(len(OSTs)):
-        evm_name = evm_names[i]
-        #print(evm_name)
+        hidden_content, gallery_content = {'KNN': [], 'EVM': []}, {'KNN': [], 'EVM': []}
+        for i in range(len(OSTs)):
+            evm_name = evm_names[i]
+            #print(evm_name)
 
-        eval_hidden = eval_evm(estimator_content(hidden, evm_name))
-        eval_hidden.update({'x': OSTs[i]})
-        hidden_content['EVM'].append(eval_hidden)
-        
-        eval_gallery = eval_evm(estimator_content(gallery, evm_name))
-        eval_gallery.update({'x': OSTs[i]})
-        gallery_content['EVM'].append(eval_gallery)
+            eval_hidden = eval_evm(estimator_content(hidden, evm_name))
+            eval_hidden.update({'x': OSTs[i]})
+            hidden_content['EVM'].append(eval_hidden)
 
-    many = 10
-    for i in range(0, many+1):
-        knn_thresh = i / many
-        #print("KNN", knn_thresh)
+            eval_gallery = eval_evm(estimator_content(gallery, evm_name))
+            eval_gallery.update({'x': OSTs[i]})
+            gallery_content['EVM'].append(eval_gallery)
 
-        eval_hidden = eval_knn(estimator_content(hidden, 'KNN'), knn_thresh)
-        eval_hidden.update({'x': knn_thresh})
-        hidden_content['KNN'].append(eval_hidden)
-        
-        eval_gallery = eval_knn(estimator_content(gallery, 'KNN'), knn_thresh)
-        eval_gallery.update({'x': knn_thresh})
-        gallery_content['KNN'].append(eval_gallery)
+        many = 10
+        for i in range(0, many+1):
+            knn_thresh = i / many
+            #print("KNN", knn_thresh)
 
-    #print(gallery_content, len(gallery_content['KNN']), len(gallery_content['EVM']))
-    #print(hidden_content, len(hidden_content['KNN']))
+            eval_hidden = eval_knn(estimator_content(hidden, 'KNN'), knn_thresh)
+            eval_hidden.update({'x': knn_thresh})
+            hidden_content['KNN'].append(eval_hidden)
 
-    write_content(os.path.join(results_path, '10pp', 'knn_gallery_test.csv'), gallery_content['KNN'])
-    write_content(os.path.join(results_path, '10pp', 'knn_hidden_test.csv'), hidden_content['KNN'])
+            eval_gallery = eval_knn(estimator_content(gallery, 'KNN'), knn_thresh)
+            eval_gallery.update({'x': knn_thresh})
+            gallery_content['KNN'].append(eval_gallery)
 
-    write_content(os.path.join(results_path, '10pp', 'evm_gallery_test.csv'), gallery_content['EVM'])
-    write_content(os.path.join(results_path, '10pp', 'evm_hidden_test.csv'), hidden_content['EVM'])
+        #print(gallery_content, len(gallery_content['KNN']), len(gallery_content['EVM']))
+        #print(hidden_content, len(hidden_content['KNN']))
 
-    
-    #print(estimator_content(hidden, 'KNN'))
+        write_content(os.path.join(results_path, exp, 'knn_gallery_test.csv'), gallery_content['KNN'])
+        write_content(os.path.join(results_path, exp, 'knn_hidden_test.csv'), hidden_content['KNN'])
+
+        write_content(os.path.join(results_path, exp, 'evm_gallery_test.csv'), gallery_content['EVM'])
+        write_content(os.path.join(results_path, exp, 'evm_hidden_test.csv'), hidden_content['EVM'])
+
+
+        #print(estimator_content(hidden, 'KNN'))
 
 
 
