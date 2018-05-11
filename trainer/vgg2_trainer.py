5289f3fb94706dc13b8e5048fe5c43b83ea76f2a
José Pedro Martinez <brightjotaxiv@gmail.com>
2018-05-11 14:03:06 -0300

    UPD: mostly cleaning a lot of code

--- trainer/vgg2_trainer.py
+++ trainer/vgg2_trainer.py
@@ -26,17 +26,22 @@ def print_dict(d):
     #for k,v in d.items():
         print("[%s] \n %s \n" % (k,v))
 
+def vprint(s, v):
+    if v:
+        print(s)
+
 def measure_call(call, n_samples=1000, verbose=False):
     times = []
     for i in range(n_samples):
         start = time.time()
         call()
         end = time.time()
+
         times += [1000 * (end - start)] # to ms
 
     if verbose:
         print(times)
-    
+
     times = times[1:]
     return np.mean(times), np.var(times)
 
@@ -45,10 +50,6 @@ def pickle_load(path):
         return pickle.load(pickle_in)
 
 def get_classifier_data(data):
-    '''
-    X = np.array(list(map(lambda x: pickle_load(x['descriptor_path']), data)))
-    Y = np.array(list(map(lambda x: np.array(x['label']), data)))
-    '''
     X, Y = data
     return X, Y
 
@@ -121,13 +122,9 @@ def experiment(samples_per_person:int, persons:int):
     
     train_data, test_data = gallery_dataset.get_training_data(samples_per_person, samples_test=5, persons_limit=persons) # this gives persons * 5 gallery test samples
 
-    
-    #dataloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True, num_workers=4)
-
-
     estimators = { # this is a dict estimator like a data type (e.g return of expand_evm())
-        'EVM': EVM(use_gpu=True, open_set_threshold=0.0),
-        'KNN': KNN(1)
+        'EVM': EVM(open_set_threshold=0.0),
+        'KNN': KNN(n_neighbors=1)
     }
 
     
@@ -149,16 +146,6 @@ def experiment(samples_per_person:int, persons:int):
             train(estim, train_data, verbose=True)
             save_model(name+'.pkl', estim)
 
-    '''
-    # dataloader train
-    for i, data in enumerate(dataloader, 0):
-        X,Y = data
-        print(X, Y)
-
-        evm.fit(X.numpy(),Y.numpy())
-        print('[%d] fit' % (i))
-    '''
-
     # logging test
     OSTs = [0.0, 0.001, 0.005, 0.01, 0.02]
     test_estimators = expand_evm(estimators['EVM'], OSTs)
@@ -170,24 +157,18 @@ def experiment(samples_per_person:int, persons:int):
 
 
 def performance_eval(estim, fit_data, predict_data, n_samples:int, verbose=True):
-    def vprint(s):
-        if verbose:
-            print(s)
     
-    
-
-    #estim = EVM(open_set_threshold=0.01)
 
     fit_x, fit_y = fit_data
-    vprint("doing a fit")
+    vprint("doing a fit", verbose)
     fit_t = measure_call(lambda: estim.fit(fit_x, fit_y), verbose=True, n_samples=5)#n_samples)
-    vprint(fit_t)
+    vprint(fit_t, verbose)
 
     
     pred_x, _ = predict_data    
-    vprint("doing a predict")
+    vprint("doing a predict", verbose)
     pred_t = measure_call(lambda: estim.predict(pred_x), n_samples=n_samples)
-    vprint(pred_t)
+    vprint(pred_t, verbose)
 
     return {'fit': fit_t, 'pred': pred_t}
 
@@ -200,142 +181,32 @@ def performance_multieval(estims:dict, samples_per_person:int, persons:int, n_sa
     
     performance = {}
     for k in sorted(models.keys()):
-        print("\nEvaluating %s with %d persons and %d samples-per-person" % (k, persons, samples_per_person))
+        print("\nEvaluating %s with %d persons and %d samples-per-person (%d total samples)" % (k, persons, samples_per_person, persons*samples_per_person))
         performance.update({k: performance_eval(models[k], fit_data, predict_data, n_samples=100, verbose=verbose)})
 
     print_dict(performance)
     
     
 if __name__ == '__main__':
-    #models = {("EVM_Red_%4d" % int(r)): EVM(redundancy_rate=r/1000.) for r in range(0, 1000, 25)}
-    models = {("EVM_BiasD_%4d" % int(r)): EVM(redundancy_rate=r/1000., biased_distance=r/1000.) for r in range(400, 1000, 50)}
-    models.update({"KNN": KNN(n_neighbors=1)})
-    #print(models)
+    mode = 'performance'
+    mode = 'experiment'
 
-    performance_multieval(models, 20, 1500, n_samples=100, verbose=True)
-    '''
-    performance = {
-        'KNN': performance_eval(load_model('KNN.pkl'),n_samples=100),
-        'EVM': performance_eval(load_model('EVM.pkl'),n_samples=100)
-    }
-    print(performance)
-    '''
-    '''
-    for pp in [10, 20, 50]:
-        experiment(pp, 8000)
-    '''
-    
-    
+    if mode == 'performance':
+        #models = {("EVM_Red_%4d" % int(r)): EVM(redundancy_rate=r/1000.) for r in range(0, 1000, 25)} # just redundancy
+        models = {("EVM_BiasD_%4d" % int(r)): EVM(redundancy_rate=r/1000., biased_distance=r/1000.) for r in range(400, 1000, 50)} # biased distance (needs to change redundancy as well)
 
-    '''
-    # direct test
-    test_evm(evm, test_data, estim_name='EVM', dataset_name='Known people', verbose=True)
-    test_knn(knn, test_data, estim_name='KNN', dataset_name='Known people')
-    test_evm(evm, hidden_dataset.data, estim_name='EVM', dataset_name='New people')
-    test_knn(knn, hidden_dataset.data, estim_name='KNN', dataset_name='New people')
+        models.update({"KNN": KNN(n_neighbors=1)})
+        #print(models)
 
-    # grid stuff
-    evm_params = {
-        'open_set_threshold': list(map(lambda x: x/float(1000), range(1, 1000, 100))),
-        'tail': range(10, 1000, 10)
-    }
-    scorers = {
-        'accuracy': make_scorer(accuracy_score),
-        #'precision': 'precision'
-    }
+        persons = 15
+        per_person_samples = 20
+        performance_multieval(models, per_person_samples, persons, n_samples=100, verbose=True)
 
-    evm_grid = GridSearchCV(evm, param_grid=evm_params, scoring=scorers, n_jobs=-1, refit='accuracy')
-    
-    train(evm_grid, dataset.train_data, verbose=True)
-
-    test(evm_grid, dataset.test_data, estim_name='EVM_grid', dataset_name='Known people')
-    test(evm_grid, (hidden_dataset.train_data + hidden_dataset.test_data), estim_name='EVM_grid', dataset_name='New people')
-    '''
-
-    
-    
-    
-"""
-def test_evm(estim, data, verbose=False, estim_name='Estimator', dataset_name='Dataset'):
-    X, Y = get_classifier_data(data)
-    Y_ = estim.predict(X)
-    if verbose: print('Test shapes', X.shape, Y.shape, Y_.shape)
-
-    if isinstance(estim, GridSearchCV):
-        print("Best params: %s" % (
-            estim.best_params_
-        ))
-    
-    total = Y.shape[0]
-    correct = (Y_ == Y).sum()
-    unknowns = (Y_ == 'Unknown').sum()
-    mismatches = total - (correct + unknowns)
-
-    #precision = skmetrics.precision_score(Y, Y_)
-    #recall = skmetrics.recall_score(Y, Y_)
-    
-    print("[%s/%s] Properly recognized: %f%% ; Unknowns/False-Negatives: %f%% ; False-Positives/Mismatches: %f%% ; Samples: %d\n" % (
-        estim_name, dataset_name,
-        100*correct/float(total),
-        100*unknowns/float(total),
-        100*mismatches/float(total),
-        total
-    ))
-
-    return {
-        'total': total,
-        'correct': correct,
-        'unknowns': unknowns,
-        'mismatches': mismatches
-    }
-
-def test_knn(estim, data, k_thresh=0.6, verbose=False, estim_name='KNN', dataset_name='Dataset'):
-    X, Y = get_classifier_data(data)
-    Y_ = estim.predict(X)
-    Y_k = estim.kneighbors(X)
-
-    total = Y.shape[0]
-    correct, unknowns, mismatches = 0, 0, 0
-    for i in range(total):
-        if Y_k[0][i] > k_thresh:
-            unknowns += 1
-        else:
-            if Y_[i] == Y[i]:
-                correct += 1
-            else:
-                mismatches += 1
+    if mode == 'experiment':
+        persons = 8000
+        per_person_samples = [10, 20, 50]
         
-    print("[%s/%s] Properly recognized: %f%% ; Unknowns/False-Negatives: %f%% ; False-Positives/Mismatches: %f%% ; Samples: %d\n" % (
-        estim_name, dataset_name,
-        100*correct/float(total),
-        100*unknowns/float(total),
-        100*mismatches/float(total),
-        total
-    ))
-
-    return {
-        'total': total,
-        'correct': correct,
-        'unknowns': unknowns,
-        'mismatches': mismatches
-    }
-      
-        
-
-def DIR_test(estim, data, verbose=False, estim_name='Estimator', dataset_name='Dataset'):
-    X, Y = get_classifier_data(data)
-    Y_ = estim.predict_proba(X)
-
-    #print("Achou ", Y_, len(Y_))
+        for pp in per_person_samples:
+            experiment(pp, persons)
     
-    pos, neg = pos_neg_from_prediction(Y, Y_)
-    dirs = all_dirs(pos, neg)
 
-    print(min(neg), min(pos))
-    print("[%s/%s] DIRs: %s" % (
-        estim_name, dataset_name,
-        dirs
-    ))
-    
-    return dirs
-"""
